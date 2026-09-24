364beb6b420a1ec97485b9f45d9df379b3f1b59c
Jerry Min <jerryjmin7@gmail.com>
2026-09-24 22:07:32 +0100

    2.7

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main()
-{
+void print_add(int, int);
+int return_5();
+
+
+int main() {
+    std::cout << return_5();
     
+    return 0;
+}
+
+int return_5() {
+
+    print_add(5, 6);
+    return 5;
+}
+
+void print_add(int n, int y) {
+    std::cout << n+y << '\n';
 }
